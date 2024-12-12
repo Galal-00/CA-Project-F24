6ee3ce1d3ae3244f74b800fce16c44b961c624be
Hussein Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2024-12-12 10:38:29 +0200

    starting point for alu

--- PipePrcsr/src/03-EX/ALU.vhd
+++ PipePrcsr/src/03-EX/ALU.vhd
@@ -3,6 +3,14 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 USE ieee.math_real.ALL;
 
+-- NOP  00000      
+-- HLT  00001      
+-- SETC  00010      
+-- NOT  00011 Rsrc1  Rdst   
+-- INC  00100 Rsrc1  Rdst   
+-- OUT  00101 Rsrc1     
+-- IN  00110   Rdst   
+
 ENTITY ALU IS
     PORT (
         -- Inputs
@@ -14,3 +22,16 @@ ENTITY ALU IS
 
     );
 END ENTITY ALU;
+
+ARCHITECTURE ALU_arch OF ALU IS
+    SIGNAL temp1 : IN STD_ULOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL temp2 : IN STD_ULOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL tempRes : OUT STD_ULOGIC_VECTOR(15 DOWNTO 0)
+
+BEGIN
+
+    WITH OpCode SELECT
+        tempRes <= < value > WHEN < sel_value >,
+        < value > WHEN OTHERS;
+
+END ARCHITECTURE;
